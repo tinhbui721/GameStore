a9d33b7b8d4e54d89e8697aece7b3426367b39b5
HieuNV <hieunv8501@gmail.com>
2022-12-20 23:54:23 +0700

    About & ExchangePolicy UI (some conflict styles)

---
+++ GameStore.WebUI/Controllers/CommonController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using System.Web;
+using GameStore.WebUI.Controllers;
+using System.Web.Mvc;
+using Microsoft.Owin.Security.DataProtection;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace GameStore.WebUI.Controllers
+{
+    public class CommonController : BaseController
+    {
+        // GET: Common
+        public ActionResult About(int code = 0)
+        {
+            ViewBag.Title = "About";
+            ViewBag.code = code;
+            return View();
+        }
+
+        public ActionResult ExchangePolicy(int code = 0)
+        {
+            ViewBag.Title = "Exchange Policy";
+            ViewBag.code = code;
+            return View();
+        }
+    }
+}
